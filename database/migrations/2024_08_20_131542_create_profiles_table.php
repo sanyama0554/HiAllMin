7058dab7c240b1081038c1ae4819d3a54d4ba2f0
YumaYamasaki <yuma.yamasaki554@gmail.com>
2024-08-20 22:41:52 +0900

    create migration files

---
+++ database/migrations/2024_08_20_131542_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->primary();
+            $table->string('name', 100);
+            $table->integer('age')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->string('location', 100)->nullable();
+            $table->text('bio')->nullable();
+            $table->text('interests')->nullable();
+            $table->text('languages_spoken')->nullable();
+            $table->string('profile_picture', 255)->nullable();
+            $table->text('visited_places')->nullable();
+            $table->text('travel_preferences')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
---
+++ database/migrations/2024_08_20_131543_create_progress_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('progress', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('assignment_id');
+            $table->enum('status', ['not_started', 'in_progress', 'completed']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('assignment_id')->references('id')->on('assignments')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('progress');
+    }
+};
